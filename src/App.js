5e560e706187a5de940c8787dbe85d4d389d961a
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-08 00:03:33 +0530

    Created basic routing schema.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { BookList } from "./pages/BookList";
+import { About } from "./pages/About";
+import { Book } from "./pages/Book";
+import { Contact } from "./pages/Contact";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Book List</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/books/new">New Book</Link>
+          </li>
+          <li>
+            <Link to="/notfound">Not Found</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} /> */}
+        {/* It can also be done using Nested Routes. */}
+        <Route path="/books">
+          <Route index element={<BookList />} />
+          <Route path=":id" element={<Book />} />
+          <Route path="new" element={<NewBook />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} /> {/*Runs if nothing matched */}
+      </Routes>
+    </>
   );
 }
 
