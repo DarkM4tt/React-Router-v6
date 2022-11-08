9a445a271941aecdd321ac79917357beb601196e
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-09 01:21:13 +0530

    Added a Layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
+import { NavLink, Link, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
 import { About } from "./pages/About";
@@ -7,6 +7,7 @@ import { Book } from "./pages/Book";
 import { Contact } from "./pages/Contact";
 import { NewBook } from "./pages/NewBook";
 import { NotFound } from "./pages/NotFound";
+import { BookLayout } from "./BookLayout";
 
 function App() {
   return (
@@ -17,7 +18,15 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/books">Book List</Link>
+            <NavLink
+              style={({ isActive }) => {
+                return isActive ? { color: "red" } : {};
+              }}
+              to="/books"
+            >
+              Book List
+            </NavLink>
+            {/* Navlink is same as link but has extra isActive property to set styles when it is active. */}
           </li>
           <li>
             <Link to="/about">About</Link>
@@ -39,7 +48,7 @@ function App() {
         <Route path="/books/:id" element={<Book />} />
         <Route path="/books/new" element={<NewBook />} /> */}
         {/* It can also be done using Nested Routes. */}
-        <Route path="/books">
+        <Route path="/books" element={<BookLayout />}>
           <Route index element={<BookList />} />
           <Route path=":id" element={<Book />} />
           <Route path="new" element={<NewBook />} />
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
 export function NotFound() {
-  return <h1>NotFound</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 4000);
+  }, []);
+  //Navigates back to home after 2 seconds.
+  return <h1>Not Found!</h1>;
+
+  // <Navigate to="/" />; By this we can navigate back to homepage.
 }
